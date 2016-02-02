153a3b32788fbc98ae2478455233b3512446060a
Gustavo Daud <gustavodaud@Gustavos-MacBook-Pro.local>
2016-02-02 20:24:48 -0200

    .

--- servicos/usuario.js
+++ servicos/usuario.js
@@ -142,10 +142,14 @@ function enviarConvite(req, res) {
                 if (err) 
                     //Caso tenha ocorrido um erro ao tentar adicionar a entrada
                     res.json({erro: "erroaoconvidar"})
-                else 
+                else {
+                    //Prepara variáveis que serão usadas
+                    var nome = (rows.length == 0) ? dados.nome : rows[0].Nome;
+                    var id = (rows.length == 0) ? null : rows[0].id;
                     //Tudo funcionou bem, retorna
-                    res.json({ok: true, dados: {Nome: rows[0].Nome || dados.nome, Id: rows[0].id, Aprovado: false}, 
-                        existe: rows.length > 0})                
+                    res.json({ok: true, dados: {Nome: nome, Id: id, Aprovado: false}, 
+                        existe: rows.length > 0})          
+                }      
             })
         }
     });
