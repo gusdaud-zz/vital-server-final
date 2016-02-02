62fe8924dbacf425818a0e95b8277868c7ec460d
Gustavo Daud <gustavodaud@Gustavos-MacBook-Pro.local>
2016-02-02 20:44:21 -0200

    .

--- servicos/usuario.js
+++ servicos/usuario.js
@@ -146,6 +146,7 @@ function enviarConvite(req, res) {
                     //Prepara variáveis que serão usadas
                     var nome = (rows.length == 0) ? dados.nome : rows[0].Nome;
                     var id = (rows.length == 0) ? null : rows[0].Id;
+                    console.log(rows[0]);
                     //Tudo funcionou bem, retorna
                     res.json({ok: true, dados: {Nome: nome, Id: id, Aprovado: false}, 
                         existe: rows.length > 0})          
