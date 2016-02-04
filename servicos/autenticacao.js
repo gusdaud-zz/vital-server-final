c696eb5b8f31716ad919f9e31466b7157b3a0400
Gustavo Daud <gustavodaud@Gustavos-MacBook-Pro.local>
2016-02-04 13:48:09 -0200

    .

--- servicos/autenticacao.js
+++ servicos/autenticacao.js
@@ -14,7 +14,7 @@ var twilio = require('twilio')(config.twilio.sid, config.twilio.token);
 
 /* ##### Para não enviar mensagens pelo twilio até que esteja em produção ##### */
 twilio.sendMessage = function(dados, callback) {
-    console.log("O SMS para " + dados.to + " com a mensagem '" + dados.body + "' não foi enviada propositalmente."  );
+    console.log("O SMS para " + dados.to + " com a mensagem '" + dados.body + "' não foi enviada propositalmente." );
     callback(null, true);
 }
 
