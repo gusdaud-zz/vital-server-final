73484a30a6d1e97507fbf525cb9a4c9cb64db1cb
Gustavo Daud <gustavodaud@Gustavos-MacBook-Pro.local>
2016-02-09 13:09:31 -0200

    .

--- app.bat
+++ app.bat
@@ -1,4 +1,12 @@
 @echo off
+
+IF "%~1"=="–FIX_CTRL_C" (
+SHIFT
+) ELSE (
+CALL <NUL %0 –FIX_CTRL_C %*
+GOTO :EOF
+)
+
 echo --------- Atualizando codigo
 git pull vital master
 echo --------- Executando NodeJS em modo debug na porta 46979
