12d347d85ef44d965be4405ecea28cac9c1c949d
Gustavo Daud <gustavodaud@Gustavos-MacBook-Pro.local>
2016-02-09 12:31:57 -0200

    .

--- app.bat
+++ app.bat
@@ -1,5 +1,5 @@
 echo off
-echo Atualizando código
-nuget pull vital master > NULL
+echo Atualizando codigo
+git pull vital master > NULL
 echo Executando NodeJS
 node --debug-brk=46979 --nolazy app.js 
