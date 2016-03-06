26b8aaeae8735017f8f2ae5106ec49f2d0298353
Gustavo Daud <gustavodaud@Gustavos-MacBook-Pro.local>
2016-03-06 11:09:34 -0300

    .

--- servicos/dispositivo.js
+++ servicos/dispositivo.js
@@ -52,7 +52,7 @@ function atualizarDispositivo(req, res) {
             if (rows[1].length == 0) return;
             for (var i in rows[1]) {
                 apn.pushNotification({expiry: Math.floor(Date.now() / 1000) + 3600, 
-                    "content-available": 1, payload: { 'tipo': "geolocalizacao", 'id': rows[i][i].Id,
+                    "content-available": 1, payload: { 'tipo': "geolocalizacao", 'id': rows[1][i].Id,
                     'latitude': req.body.latitude, 'longitude': req.body.longitude }}, rows[1][i].Push);
             }
         }
